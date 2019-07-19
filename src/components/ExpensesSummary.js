c44fd045e9168dfce8b3e31e98cdb5bad414bf84
Tuong Nguyen <tuongnyc@outlook.com>
2019-07-19 14:27:21 -0400

    fix the spacing in dashboard

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -7,10 +7,10 @@ import selectExpensesTotal from '../selectors/expenses-total';
 // expenseCount, expensesTotal
 export const ExpensesSummary = (props) => {
     const formattedExpensesTotal = numeral(props.expensesTotal/100).format('$0,0.00')
-    const wordExpense = props.expenseCount > 1 ? 'expenses' : 'expense';
+    const wordExpense = props.expenseCount > 1 ? ' expenses' : ' expense';
     return (
         <div>
-            <h1> viewing {props.expenseCount} 
+            <h1> Viewing {props.expenseCount}
             {wordExpense} totalling {formattedExpensesTotal} </h1>
         </div>
     );
