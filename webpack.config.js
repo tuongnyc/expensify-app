92efea3a6ce4b0a4fea95213c941af5de7bc3184
Tuong Nguyen <tuongnyc@outlook.com>
2019-07-19 09:49:09 -0400

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,52 @@
+// NodeJS file.  no import, use require!
 const path = require('path');
 
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CSSExtract = new ExtractTextPlugin('styles.css');
+
+module.exports = (env) => {
+  const isProduction = env === 'production';
+
+  return {
+    //entry: './playground/hoc.js',
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: { sourceMap: true }
+            },
+            {
+              loader: 'sass-loader',
+              options: { sourceMap: true }
+            }
+          ]
+        })
+      }]
+    }, 
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map', //'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
+  };  
+};
+
+/*
 module.exports = {
   //entry: './playground/hoc.js',
   entry: './src/app.js',
@@ -27,4 +74,4 @@ module.exports = {
     historyApiFallback: true
   }
 };
-
+*/
